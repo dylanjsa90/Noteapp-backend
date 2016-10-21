fde496566e06b6c00a4f9103a1de9a0ab8287c8b
dylanjsa90 <dylanjsanders1@gmail.com>
2016-10-20 17:45:20 -0700

    removed name from note model and validation from list method

--- model/list.js
+++ model/list.js
@@ -19,7 +19,7 @@ const listSchema = mongoose.Schema({
 listSchema.methods.addNote = function(data){
   let result;
   return new Promise((resolve, reject) => {
-    if (!data.name || !data.content || !data.listId)
+    if (!data.content || !data.listId)
       return reject(createError(400, 'note requires name, content, and listId'));
     new Note(data).save()
       .then(note => {
